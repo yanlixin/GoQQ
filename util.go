8ec1e0c5921f3be8dfb3b141eba3f2ffe38a2659
David Yan <yan.lixin@gmail.com>
2015-10-16 08:41:50 +0000

    hash 1

--- util.go
+++ util.go
@@ -108,9 +108,17 @@ func getColorLevel(level string) string {
 	}
 	return level
 }
+func charCodeAt(s string, n int) rune {
+	for i, r := range s {
+		if i == n {
+			return r
+		}
+	}
+	return 0
+}
 func CalcHash(x string, K string) string {
 	//x = str(x);
-	N := K + "password error"
+	/*N := K + "password error"
 	T := ""
 	var V []int
 	for {
@@ -128,23 +136,8 @@ func CalcHash(x string, K string) string {
 	for U, _ := range T {
 		V = append(V, int(T[U])^int(N[U]))
 	}
-    x += ""
-    for (var N = [], T = 0; T < K.length; T++) {
-    	N[T % 4] ^= K.charCodeAt(T);
-    }
-    var U = [
-      'EC',
-      'OK'
-    ],
-    V = [
-    ];
-    V[0] = x >> 24 & 255 ^ U[0].charCodeAt(0);
-    V[1] = x >> 16 & 255 ^ U[0].charCodeAt(1);
-    V[2] = x >> 8 & 255 ^ U[1].charCodeAt(0);
-    V[3] = x & 255 ^ U[1].charCodeAt(1);
-    U = [
-    ];
-	
+
+
 	N1 := []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"}
 	T = ""
 	for U, _ := range V {
@@ -152,5 +145,52 @@ func CalcHash(x string, K string) string {
 		T += N1[V[U]&15]
 	}
 	return T
+	*/
+	//x += ""
+	var N []rune
+	for T := 0; T < len(K); T++ {
+		N[T%4] ^= charCodeAt(K, T)
+	}
+	U := []string{
+		"EC",
+		"OK",
+	}
+	var V []rune
+	V[0] = x>>24&255 ^ charCodeAt(U[0], 0)
+	V[1] = x>>16&255 ^ charCodeAt(U[0], 1)
+	V[2] = x>>8&255 ^ charCodeAt(U[1], 0)
+	V[3] = x&255 ^ charCodeAt(U[1], 1)
+	var U []rune
+	for T = 0; T < 8; T++ {
 
+		if T%2 == 0 {
+			U[T] = N[T>>1]
+		} else {
+			U[T] = V[T>>1]
+		}
+	}
+	N1 := []string{
+		'0',
+		'1',
+		'2',
+		'3',
+		'4',
+		'5',
+		'6',
+		'7',
+		'8',
+		'9',
+		'A',
+		'B',
+		'C',
+		'D',
+		'E',
+		'F',
+	}
+	V1 := ""
+	for T = 0; T < U.length; T++ {
+		V1 += N1[U[T]>>4&15]
+		V1 += N1[U[T]&15]
+	}
+	return V
 }
